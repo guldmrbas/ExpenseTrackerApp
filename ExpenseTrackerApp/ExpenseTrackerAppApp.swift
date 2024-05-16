0a7768ed50db980ad26fb631cccdcf8311d6caf4
Gülsüm Demirbaş <gdem2334@gmail.com>
2024-05-16 17:10:18 +0300

    Initial Commit

---
+++ ExpenseTrackerApp/ExpenseTrackerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerAppApp.swift
+//  ExpenseTrackerApp
+//
+//  Created by Gülsüm Demirbaş on 16.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
