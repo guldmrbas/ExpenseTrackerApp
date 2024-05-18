71107fb3584330ce44e82e6dd96aee80a89bf58a
Gülsüm Demirbaş <gdem2334@gmail.com>
2024-05-18 16:19:36 +0300

    first commit

--- ExpenseTrackerApp/ContentView.swift
+++ ExpenseTrackerApp/ContentView.swift
@@ -6,19 +6,72 @@
 //
 
 import SwiftUI
+import SwiftUICharts
 
 struct ContentView: View {
+    @EnvironmentObject var transactionListVM : TransactionListViewModel
+    
+    //var demoData: [Double] = [8, 2, 4, 6, 12, 9, 2]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView{
+            ScrollView{
+                VStack(alignment:.leading , spacing: 24){
+                    Text("Overview")
+                        .font(.title2)
+                        .bold()
+                    // Mark : Chart
+                    let data = transactionListVM.accumulateTransaction()
+                    if !data.isEmpty{
+                        let totalExpenses = data.last?.1 ?? 0
+                        
+                        CardView {
+                            VStack(alignment: .leading){
+                                ChartLabel(totalExpenses.formatted(.currency(code: "USD")), type: .title , format: "$%.02f")
+                                
+                                LineChart()
+                            }
+                            .background(Color.systemBackground)
+                        }
+                                .data(data)
+                                .chartStyle(ChartStyle(backgroundColor: Color.systemBackground, foregroundColor: ColorGradient(Color.icon.opacity(0.4) , Color.icon)))
+                            .frame(height: 300)
+                            
+                        
+                    }
+                    
+                   
+                    
+                    
+                    //Mark : Transaction List
+                    RecentTransactionList()
+                        
+                }
+                .padding()
+                .frame(maxWidth : .infinity)
+            }
+            .background(Color.background)
+            .navigationViewStyle(.stack)
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem{
+                    Image(systemName: "bell.badge")
+                        .renderingMode(.original)
+                        .symbolRenderingMode(.palette)
+                        .foregroundStyle(Color.icon, .primary)
+                }
+            }
+            
         }
-        .padding()
+        .navigationViewStyle(.stack)
+        .accentColor(.primary)
     }
 }
+    
 
 #Preview {
-    ContentView()
+    let transactionListVM = TransactionListViewModel()
+        transactionListVM.transactions = transactionListPreviewData
+        
+        return RecentTransactionList()
+            .environmentObject(transactionListVM)
 }
